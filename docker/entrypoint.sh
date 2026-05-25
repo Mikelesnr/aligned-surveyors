2a3fabacd58c47dd5022e8686673e5112c3e0492
Michael Mwanza <mwanza.n.m@gmail.com>
2026-05-25 17:26:13 +0200

    dockerfile update

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
-# Render provides $PORT, default to 8080
+# Use the PORT provided by Render, default to 8080
 PORT="${PORT:-8080}"
 
-# Run discovery and cache clear at runtime
+# Replace listen 80; in your nginx.conf with the Render PORT
+sed -i "s/listen 80;/listen $PORT;/g" /etc/nginx/nginx.conf
+
+# Run package discovery and cache commands at runtime
 php artisan package:discover --ansi
 php artisan config:cache
 php artisan route:cache
-php artisan view:cache
-
-# Update Nginx port
-sed -i "s/listen 80;/listen $PORT;/g" /etc/nginx/nginx.conf
 
 # Start Supervisor
 exec /usr/bin/supervisord -c /etc/supervisord.conf
