2375dfcda4b465d0121ed6a822b4ee8d4ffa60c4
Michael N Mwanza <mwanza.n.m@gmail.com>
2026-06-01 22:57:56 +0200

    brocken script tag removed

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -41,25 +41,7 @@
         <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png">
         <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png">
         <link rel="manifest" href="/images/site.webmanifest">
-        <script defer src="https://meet.jit.si/external_api.js" defer></script>
-        <script type="application/ld+json">
-            {
-            "@context": "https://schema.org",
-            "@type": "LocalBusiness",
-            "name": "Aligned Surveyors",
-            "url": "https://alignedsurveyors.co.zw",
-            "logo": "https://alignedsurveyors.co.zw/images/logo.png",
-            "founder": {
-                "@type": "Person",
-                "name": "Kuzivakwashe Kennedy Nyandoro",
-                "jobTitle": "Principal / Owner",
-                "sameAs": "https://www.linkedin.com/in/kuzivakwashe-kennedy-nyandoro-3b827a93/"
-            },
-            "sameAs": [
-                "https://www.linkedin.com/in/kuzivakwashe-kennedy-nyandoro-3b827a93/"
-            ]
-            }
-        </script>
+        <script src="https://meet.jit.si/external_api.js" defer></script>
 
         <!-- Scripts -->
         @routes
